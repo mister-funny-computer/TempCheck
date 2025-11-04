ffe2143b1351faf5e595547be7765794039d15ef
Mister funny computer <tironasaurusnik@gmail.com>
2025-11-04 19:21:49 +0300

    program done

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ while True:
             print("прохладно")
         elif 0 < temp <= 12:
             print("чуть теплее нуля")
+        elif 12 < temp <= 18:
+            print("тeпло")
+        elif 18 < temp <= 25:
+            print("теплее тёплого")
+        elif 25 < temp <= 32:
+            print("Жарко")
+        elif temp > 32:
+            print("Умираю от жары не от холода ")
